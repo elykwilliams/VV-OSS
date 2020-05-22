a2053a92bed8f9514e814977b605bb04e5a55ae1
kyle <williams.kyle@gmx.com>
2020-05-21 21:06:54 -0500

    Auto Update all prm files

---
+++ test/prm/update_prm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+#include "Base/settings.h"
+
+
+int main (int argc, char *argv[]){
+
+    for(int i=1; i < argc; ++i) {
+        string parameter_file{argv[i]};
+
+        try {
+            Settings settings(parameter_file);
+        }
+
+        catch (std::exception &exc) {
+
+            std::cerr << std::endl << std::endl
+                      << "----------------------------------------------------"
+                      << std::endl;
+            std::cerr << "Exception on processing: " << parameter_file << std::endl << exc.what()
+                      << "----------------------------------------------------"
+                      << std::endl;
+
+            return 1;
+        }
+
+    }
+}
